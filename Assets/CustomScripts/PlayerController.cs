755658d0bc1b422c8f3ada5a6d54842d4bfb4d49
Yasir Nadeem <m-yasir.dev@protonmail.com>
2020-05-12 08:56:18 +0500

    Added player in the scene. Added player controller script to move the player. Changed camera to be the child of player. Added MouseLook script (From standard assets) to player.

---
+++ Assets/CustomScripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementMultiplier = 1.0f; // Movement Speed controller
+    public float gravityMultiplier = 9.8f; // Player fall Speed controller
+
+    private CharacterController myController;
+
+    private void Start()
+    {
+        myController = gameObject.GetComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // create transform vectors for horizontal and vertical movement
+        Vector3 moveZ = Input.GetAxis("Vertical") * Vector3.forward * movementMultiplier * Time.deltaTime;
+        Vector3 moveX = Input.GetAxis("Horizontal") * Vector3.right * movementMultiplier * Time.deltaTime;
+
+        // transform object's coordinates
+        Vector3 movement = transform.TransformDirection(moveX + moveZ);
+
+        // Apply gravity
+        movement.y -= gravityMultiplier;
+
+        myController.Move(movement);
+    }
+}
